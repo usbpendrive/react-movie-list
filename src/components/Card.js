e5ac234424db6f6bafb55f15aa88fb1a269a7de5
usbpendrive <ferry.fernando@gmail.com>
2020-06-22 20:53:29 +0700

    More basic react component.

---
+++ src/components/Card.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Card = () => {
+    return <h2>movie #1</h2>;
+}
+
+export default Card;
--- src/containers/List.js
+++ src/containers/List.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
+import Card from "../components/Card";
 
 class List extends Component {
     render() {
-        return <h1>movieList</h1>;
+        return <Card />;
     }
 };
 
