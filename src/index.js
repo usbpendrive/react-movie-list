4ad2892660fe51192dba4006f182559d9964bd1e
usbpendrive <ferry.fernando@gmail.com>
2020-06-22 20:47:20 +0700

    Add react, babel, webpack. Basic react project structure.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("movieList");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <h1>movieList</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
